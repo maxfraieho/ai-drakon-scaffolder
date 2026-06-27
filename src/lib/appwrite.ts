77543ae98d525336c154a2fffc70014d4772f099
CELESTINE GERONIMO <maxfraieho@gmail.com>
2026-06-27 15:32:03 +0300

    auch

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Account, Databases } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setEndpoint("https://auth.aidrakon.tech/v1")
   .setProject("6a23420a003a04b4997b");
 
 const account = new Account(client);
